d281190f3caecc230fdd8972ef17e0975b9c1752
xXSlothAIXx <bluehaze3295@gmail.com>
2020-12-18 12:30:53 -0500

    models manager

---
+++ penguin_backend/forum/models/__init__.py
@@ -0,0 +1,4 @@
+from .answer_model import  *
+from .category_model import *
+from .comment_model import  *
+from .post_model import *
---
+++ penguin_backend/forum/models/category_model.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class CategoryManager(models.Manager):
+
+    def get_latest_categories(self):
+        categories = self.objects.all()
+        return categories
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    date_posted = models.DateTimeField(default=timezone.now)
+    objects = CategoryManager()
+    
+    class Meta:
+        ordering = ['date_posted']
+
+    def __str__(self):
+        return f'Title: {self.title}'
